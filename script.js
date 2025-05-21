216ad0b0c16552648708dd78e06c923560aa4184
Felipe strack <159437447+strackfelipe@users.noreply.github.com>
2025-05-21 08:05:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.innerHTML = "Te amo hoje, amanhã e sempre 💕";
+    mensagem.style.display = 'block';
+}
